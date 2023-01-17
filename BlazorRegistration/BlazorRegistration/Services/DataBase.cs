e2691862a22469615843784b0664d902e18fee1e
Ruslana <garaeva-r_23@mail.ru>
2023-01-17 10:06:02 +0300

    Adding a download and timer

--- BlazorRegistration/BlazorRegistration/Services/DataBase.cs
+++ BlazorRegistration/BlazorRegistration/Services/DataBase.cs
@@ -4,24 +4,25 @@ using MongoDB.Driver;
 using BlazorRegistration.Data;
 
 namespace BlazorRegistration.Services;
-    public class DataBase
-    {
-        public User? CurrentUser { get; set; }
+public class DataBase
+{
+    public User? CurrentUser { get; set; }
         
-        public void AddToDataBase(User user)
-        {
-            MongoClient client = new MongoClient("mongodb://localhost");
-            IMongoDatabase database = client.GetDatabase("Registration");
-            var collection = database.GetCollection<User>("UsersList");
-            collection.InsertOne(user);
-        }
-        public User FindByLogin(string login)
-        {
-            MongoClient client = new MongoClient("mongodb://localhost");
-            IMongoDatabase database = client.GetDatabase("Registration");
-            var collection = database.GetCollection<User>("UsersList");
-            var user = collection.Find(x => x.Login == login).FirstOrDefault();
-
-            return user;
-        }
+    public void AddToDataBase(User user)
+    {
+        MongoClient client = new MongoClient("mongodb://localhost");
+        IMongoDatabase database = client.GetDatabase("Registration");
+        var collection = database.GetCollection<User>("UsersList");
+        collection.InsertOne(user);
     }
+    public async Task<User> FindByLoginAsync(string login)
+    {
+        await Task.Delay(3000);
+        MongoClient client = new MongoClient("mongodb://localhost");
+        IMongoDatabase database = client.GetDatabase("Registration");
+        var collection = database.GetCollection<User>("UsersList");
+        var user = collection.Find(x => x.Login == login).FirstOrDefault();
+
+        return user;
+    }
+}
