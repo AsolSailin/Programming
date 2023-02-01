c4b5398829dcb1a866d4d5b5495d1b2f5ed1e695
Ruslana <garaeva-r_23@mail.ru>
2023-02-01 08:42:35 +0300

    Edited code

--- BlazorSignalR/SignalR/ChatHub.cs
+++ BlazorSignalR/SignalR/ChatHub.cs
@@ -4,9 +4,9 @@ namespace SignalR
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string message, string userName)
+        public async Task Send(string message, string userName, string mail)
         {
-            await Clients.All.SendAsync("Receive", message, userName);
+            await Clients.All.SendAsync("Receive", message, userName, mail);
         }
     }
 }
